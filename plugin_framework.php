6c60f4985b30f9f146a242d62842d042d2073572
Matthew L <matthew@octeth.com>
2014-02-12 15:29:36 +0200

    initial commits

---
+++ plugin_framework.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Framework
+ * Name: Plugin Framework
+ * Description: Plugin development framework for Oempro Plugin Developers
+ * Minimum Oempro Version: 4.6.2
+ */
+
+include_once(PLUGIN_PATH . 'plugin_framework/main.php');
