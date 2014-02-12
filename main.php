9bc58170bc1a91e14b78a5716a743c0c16e23fd3
Cem Hurturk <cem@octeth.com>
2014-02-12 18:32:32 +0200

    working on the main plugin file

--- main.php
+++ main.php
@@ -2,9 +2,11 @@
 
 include_once(PLUGIN_PATH . 'plugin_framework/libraries/licensing.php');
 
-class plugin_famework extends Plugins
+class plugin_framework extends Plugins
 {
+	// -------------------------------------
 	// Plugin must-have variablies:
+	// -------------------------------------
 	// - Do not rename them!
 	// - Do not remove them!
 	private static $ArrayLanguage = array();
@@ -16,6 +18,7 @@ class plugin_famework extends Plugins
 	private static $PluginLicenseStatus = true;
 	private static $PluginVersion = "1.0.1";
 	private static $PluginLicenseInfo = null;
+
 
 	// -------------------------------------
 	// Default Plugin Methods
@@ -35,28 +38,32 @@ class plugin_famework extends Plugins
 
 	// This method is executed when the administrator clicks "Disable" link
 	// next to this plugin in Plugin management of the Oempro admin area
-	public function disable_octautomation()
+	public function disable_plugin_framework()
 	{
 	}
 
 	// This method is executed whenever an Oempro page (admin, user or any other pages)
 	// is executed. The plugin must be enabled to get this method executed. It's somewhat
 	// like the constructor method.
-	public function load_octautomation()
+	public function load_plugin_framework()
 	{
 	}
+
 
 	// -------------------------------------
 	// Hooks, menu item setups, template tag setups
 	// -------------------------------------
 
+
 	// -------------------------------------
 	// Controllers
 	// -------------------------------------
 
+
 	// -------------------------------------
 	// Events
 	// -------------------------------------
+
 
 	// -------------------------------------
 	// Private methods
--- plugin_framework.php
+++ plugin_framework.php
@@ -6,4 +6,15 @@
  * Minimum Oempro Version: 4.6.2
  */
 
+/*
+ * DO NOT write any code to this file. Keep it as simple as possible, especially
+ * if you are going to ioncube encrypt the source code of the plugin. This file
+ * should be kept open source, not encrypted.
+ *
+ * */
+
+// Enter the path of the main.php file inside your plugin directory.
+// PLUGIN_PATH is defined by default by Oempro and points to the
+// /path/oempro/plugins/ directory. It already includes a trailing '/'
+
 include_once(PLUGIN_PATH . 'plugin_framework/main.php');
